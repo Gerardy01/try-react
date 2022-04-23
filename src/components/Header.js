32b373fae05a0d05fe990bb02ed9e2727a08cf26
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2022-04-23 13:24:12 +0700

    membuat navMenu - items

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,20 @@ import { useState } from "react";
 
 function Header() {
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const [lastItem, setLastItem] = useState(0);
 
     const change = () => {
         if (!isOpen) {
             setIsOpen(true);
         } else {
             setIsOpen(false);
+            setLastItem(0);
         }
+    }
+
+    const handle = (lastItemNum) => {
+        setLastItem(lastItemNum);
     }
 
     return (
@@ -26,11 +32,36 @@ function Header() {
                 <div className="fav-btn">  </div>
             </div>
             <ul className={ isOpen ? "nav-open" : "nav-closed" }>
-                <NavItem title="Home" />
-                <NavItem title="Contacts" />
-                <NavItem title="Products" />
-                <NavItem title="Live" />
-                <NavItem title="About" />
+                <NavItem title="Home" handle={handle} id={1} theLastItem={lastItem}>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </NavItem>
+                <NavItem title="Contacts" handle={handle} id={2} theLastItem={lastItem}>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </NavItem>
+                <NavItem title="Products" handle={handle} id={3} theLastItem={lastItem}>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </NavItem>
+                <NavItem title="Live" handle={handle} id={4} theLastItem={lastItem}>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </NavItem>
+                <NavItem title="About" handle={handle} id={5} theLastItem={lastItem}>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                    <li>test1</li>
+                </NavItem>
             </ul>
         </div>
     )
--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react"
+
 
 function NavItem(props) {
+
+    const [navItemOpen, setNavItemOpen] = useState(false);
+
+    const navItemSet = () => {
+        if (!navItemOpen && props.children) {
+            setNavItemOpen(true);
+            props.handle(props.id);
+        } else {
+            setNavItemOpen(false);
+        }
+    }
+
+    useEffect(() => {
+        if (props.id != props.theLastItem) {
+            setNavItemOpen(false);
+        }
+    }, [props.theLastItem]);
+
     return(
-        <li className="nav-item"
-            ><a href="test"> {props.title} </a>
+        <li className="nav-item">
+
+            <div className="nav-item-header" onClick={navItemSet}>
+                <span> {props.title} </span>
+            </div>
+
+            <ul className={navItemOpen ? "nav-item-item-holder-open" : "nav-item-item-holder-closed"}>
+                {props.children}
+            </ul>
         </li>
     )
 }
